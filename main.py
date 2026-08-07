e2a1d2c532f6b05f71b2266c584631445a7bf4de
mtumae <mtume2016@gmail.com>
2026-08-07 22:00:49 +0300

    Packages into requirements.txt

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI(title="FastAPI Service")
+app = FastAPI(title="Chaturanga")
 
 
 @app.get("/")
 def read_root():
-    return {"status": "online", "message": "FastAPI is running inside Docker"}
+    return {"status": "online", "message": "FastAPI is running."}
 
 
 @app.get("/health")
